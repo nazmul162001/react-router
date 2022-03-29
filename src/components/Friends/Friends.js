2763477904d94c5f94a27f096349df434c042470
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-29 12:25:19 +0600

    creat usefull components and loaded user data from json placeholder

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+  const [friends, setFriends] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setFriends(data))
+  },[])
   return (
     <div>
-      <h2>This is our Friends page</h2>
+      <h2>Hello Friends How are you?</h2>
+      <p><small>Parle kisu taka dhar dis {friends.length} </small></p>
+      {
+        friends.map(friend => <Friend 
+        key = {friend.id}
+        friend = {friend}
+        />)
+      }
     </div>
   );
 };
